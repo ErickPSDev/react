b59d834a74c4b5b7e1316f82f4207b35652a84ff
Erick-Gitbash <licerickps@gmail.com>
2022-03-15 21:21:30 -0600

    e-commerce casi completo

--- src/Components/ProductSection/index.js
+++ src/Components/ProductSection/index.js
@@ -4,49 +4,32 @@ import Card from "react-bootstrap/Card"
 import Row from "react-bootstrap/Row"
 import Badge from "react-bootstrap/Badge"
 import Button from "react-bootstrap/Button"
+import products from "../Data/products.json"
 
-function ProductSection() {
+function ProductSection(props) {
+    const { addToCart } = props;
     return (
         <div>
-            <h4> Products </h4>
+            <h4> New Products </h4>
             <Container fluid>
                 <Row>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="https://www.shutterstock.com/image-photo/mockup-white-tshirt-blank-shirt-template-1178164783" />
-                        <Card.Body>
-                            <Card.Title>DANVOUY Womens T-shirt Casual Cotton Short</Card.Title>
-                            <Card.Text>
-                                $12.99
-                            </Card.Text>
-                            <Badge pill bg="primary">
-                                Women's Clothing
-                            </Badge>{' '}
-                        </Card.Body>
-                    </Card>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="https://www.shutterstock.com/image-photo/mockup-white-tshirt-blank-shirt-template-1178164783" />
-                        <Card.Body>
-                            <Card.Title>DANVOUY Womens T-shirt Casual Cotton Short</Card.Title>
-                            <Card.Text>
-                                $12.99
-                            </Card.Text>
-                            <Badge pill bg="primary">
-                                Women's Clothing
-                            </Badge>{' '}
-                        </Card.Body>
-                    </Card>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="https://www.shutterstock.com/image-photo/mockup-white-tshirt-blank-shirt-template-1178164783" />
-                        <Card.Body>
-                            <Card.Title>DANVOUY Womens T-shirt Casual Cotton Short</Card.Title>
-                            <Card.Text>
-                                $12.99
-                            </Card.Text>
-                            <Badge pill bg="primary">
-                                Women's Clothing
-                            </Badge>{' '}
-                        </Card.Body>
-                    </Card>
+                    {products.map((product) => { /*Cuando usamos map, siempre debe llevar key de valor unico. */
+                        console.log(product)
+                        const { id, title, price, description, category, image } = product
+                        // en iteracion se obtiene un objeto
+                        return (
+                            <Card key={id} style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src={product.image} />
+                                <Card.Body>
+                                    <Card.Title> {product.title} </Card.Title>
+                                    <Card.Text> {product.price} </Card.Text>
+                                    <Card.Text> {product.description} </Card.Text>
+                                    <Badge pill bg="primary"> {product.category} </Badge>
+                                </Card.Body>
+                            </Card>
+                        )
+                    }
+                    )}
                 </Row>
                 <Button variant="outline-dark"> View all products </Button>
             </Container>
